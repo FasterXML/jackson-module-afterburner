345a523c6c668a8ab46c94fe70242397846e6929
Tatu Saloranta <tsaloranta@gmail.com>
2011-06-21 23:23:03 -0700

    ...

--- src/main/java/com/fasterxml/jackson/module/afterburner/ser/PropertyCollector.java
+++ src/main/java/com/fasterxml/jackson/module/afterburner/ser/PropertyCollector.java
@@ -2,7 +2,11 @@ package com.fasterxml.jackson.module.afterburner.ser;
 
 import java.util.*;
 
+import static org.codehaus.jackson.org.objectweb.asm.Opcodes.*;
+
 import org.codehaus.jackson.map.ser.BeanPropertyWriter;
+import org.codehaus.jackson.org.objectweb.asm.ClassWriter;
+import org.codehaus.jackson.org.objectweb.asm.MethodVisitor;
 
 /**
  * Simple collector used to keep track of properties for which code-generated
@@ -33,12 +37,52 @@ public class PropertyCollector
     public boolean hasEntries() {
         return !(_intGetters.isEmpty());
     }
+
+    /*
+    /**********************************************************
+    /* Code generation
+    /**********************************************************
+     */
+
+    public byte[] generateAccessorClass(Class<?> beanType)
+    {
+        ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_MAXS);
+        String generatedName = beanType.getName() + "$Access4Jackson";
+        String generatedClass = internalClassName(generatedName);
+        String superClass = internalClassName(BeanPropertyAccessor.class.getName());
+        
+        // muchos important: level at least 1.5 to get generics!!!
+        cw.visit(V1_5, ACC_PUBLIC + ACC_SUPER, generatedClass, null, superClass, null);
+        cw.visitSource(generatedName + ".java", null);
+        generateDefaultConstructor(cw, superClass);
+
+        if (!_intGetters.isEmpty()) {
+            // !!! TBI
+        }
+        cw.visitEnd();
+        return cw.toByteArray();
+    }
+
+    private static void generateDefaultConstructor(ClassWriter cw, String superName)
+    {
+        MethodVisitor mv = cw.visitMethod(ACC_PUBLIC, "<init>", "()V", null, null);
+        mv.visitCode();
+        mv.visitVarInsn(ALOAD, 0);
+        mv.visitMethodInsn(INVOKESPECIAL, superName, "<init>", "()V");
+        mv.visitInsn(RETURN);
+        mv.visitMaxs(0, 0); // don't care (real values: 1,1)
+        mv.visitEnd();
+    }
     
     /*
     /**********************************************************
     /* Helper methods
     /**********************************************************
      */
+
+    private static String internalClassName(String className) {
+        return className.replace(".", "/");
+    }
     
     private <T extends OptimizedBeanPropertyWriter<T>> T _add(List<T> list, T value) {
         list.add(value);
