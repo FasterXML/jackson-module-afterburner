8058486cffa7ac6cbd1b4628030e7b0676b4fd9e
Tatu Saloranta <tsaloranta@gmail.com>
2012-11-16 19:42:33 -0800

    ...

--- profile.sh
+++ profile.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-java -Xmx64m -server -cp lib/\*:target/classes \
+java -Xmx64m -server -cp target/classes:target/test-classes:lib/\* \
  -Xrunhprof:cpu=samples,depth=10,verbose=n,interval=2 \
 $*
