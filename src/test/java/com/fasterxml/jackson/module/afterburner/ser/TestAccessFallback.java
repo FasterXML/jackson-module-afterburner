2017ba033cfa70a79df30bb39d5eaf681748dad3
Steven Schlansker <steven@likeness.com>
2013-04-24 13:13:44 -0700

    Test that we do something reasonable on IllegalAccessError

---
+++ src/test/java/com/fasterxml/jackson/module/afterburner/ser/TestAccessFallback.java
@@ -0,0 +1,54 @@
+package com.fasterxml.jackson.module.afterburner.ser;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.module.afterburner.AfterburnerTestBase;
+
+public class TestAccessFallback extends AfterburnerTestBase
+{
+    static class MyBean
+    {
+        private String e;
+
+        public MyBean() { }
+
+        MyBean(String e)
+        {
+            setE(e);
+        }
+
+        public void setE(String e)
+        {
+            for (StackTraceElement elem : new Throwable().getStackTrace()) {
+                if (elem.getClassName().contains("Access4Jackson")) {
+                    throw new IllegalAccessError("boom!");
+                }
+            }
+            this.e = e;
+        }
+
+        public String getE()
+        {
+            for (StackTraceElement elem : new Throwable().getStackTrace()) {
+                if (elem.getClassName().contains("Access4Jackson")) {
+                    throw new IllegalAccessError("boom!");
+                }
+            }
+            return e;
+        }
+    }
+
+    private static final String BEAN_JSON = "{\"e\":\"a\"}";
+
+    public void testSerializeAccess() throws Exception
+    {
+        ObjectMapper abMapper = mapperWithModule();
+        assertEquals(BEAN_JSON, abMapper.writeValueAsString(new MyBean("a")));
+    }
+
+    public void testDeserializeAccess() throws Exception
+    {
+        ObjectMapper abMapper = mapperWithModule();
+        MyBean bean = abMapper.readValue(BEAN_JSON, MyBean.class);
+        assertEquals("a", bean.getE());
+    }
+}
