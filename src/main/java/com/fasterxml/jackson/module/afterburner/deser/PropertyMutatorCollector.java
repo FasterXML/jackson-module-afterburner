090dfa256f16d1304529d76323515614f2f74c3d
Tatu Saloranta <tatu.saloranta@iki.fi>
2015-03-08 18:07:51 -0700

    Unify field-setter for if-based variant too; separate methods only for switch-based field/setter now

--- src/main/java/com/fasterxml/jackson/module/afterburner/deser/PropertyMutatorCollector.java
+++ src/main/java/com/fasterxml/jackson/module/afterburner/deser/PropertyMutatorCollector.java
@@ -225,7 +225,8 @@ public class PropertyMutatorCollector
         final boolean mustCast = parameterType.equals(OBJECT_TYPE);
 
         // First, special case: single setter
-        if (props.size() == 1) {
+        switch (props.size()) {
+        case 1:
             final OptimizedSettableBeanProperty<?> prop = props.get(0);
             _generateMutatorWrapper(cw, genClassName, methodName, parameterType, loadValueCode,
                     new MutatorGenerator() {
@@ -250,10 +251,8 @@ public class PropertyMutatorCollector
                 }
             });
             return;
-        }
-        final boolean useSwitch = (props.size() > 3);
-
-        if (!useSwitch) {
+        case 2:
+        case 3:
             _generateMutatorWrapper(cw, genClassName, methodName, parameterType, loadValueCode,
                     new MutatorGenerator() {
                 @Override
@@ -263,6 +262,8 @@ public class PropertyMutatorCollector
             });
             return;
         }
+
+        // Otherwise, use switch
         
         MethodVisitor mv = cw.visitMethod(ACC_PUBLIC, methodName, "(Ljava/lang/Object;I"+parameterType+")V", /*generic sig*/null, null);
         mv.visitCode();
@@ -289,13 +290,13 @@ public class PropertyMutatorCollector
      */
 
     private <T extends OptimizedSettableBeanProperty<T>> void _addFields(ClassWriter cw,
-            String genClassName, List<T> props,
+            String genClassName, final List<T> props,
             String methodName, Type parameterType, final int loadValueCode)
     {
         final boolean mustCast = parameterType.equals(OBJECT_TYPE);
 
-        // First, special case: single setter
-        if (props.size() == 1) {
+        switch (props.size()) {
+        case 1:
             final OptimizedSettableBeanProperty<?> prop = props.get(0);
             _generateMutatorWrapper(cw, genClassName, methodName, parameterType, loadValueCode,
                     new MutatorGenerator() {
@@ -315,6 +316,17 @@ public class PropertyMutatorCollector
                 }
             });
             return;
+
+        case 2:
+        case 3:
+            _generateMutatorWrapper(cw, genClassName, methodName, parameterType, loadValueCode,
+                    new MutatorGenerator() {
+                @Override
+                public void generate(MethodVisitor mv, int localVarIndex) {
+                    _addFieldsUsingIf(mv, props, loadValueCode, localVarIndex, mustCast);
+                }            
+            });
+            return;
         }
 
         MethodVisitor mv = cw.visitMethod(ACC_PUBLIC, methodName, "(Ljava/lang/Object;I"+parameterType+")V", /*generic sig*/null, null);
@@ -325,15 +337,8 @@ public class PropertyMutatorCollector
         mv.visitTypeInsn(CHECKCAST, beanClassName);
         mv.visitVarInsn(ASTORE, localVarIndex); // 3 args (0 == this), so 4 is the first local var slot, 5 for long
 
-        // Ok; minor optimization, 3 or fewer fields, just do IFs; over that, use switch
-        switch (props.size()) {
-        case 2:
-        case 3:
-            _addFieldsUsingIf(mv, props, loadValueCode, localVarIndex, mustCast);
-            break;
-        default:
-            _addFieldsUsingSwitch(mv, props, loadValueCode, localVarIndex, mustCast);
-        }
+        _addFieldsUsingSwitch(mv, props, loadValueCode, localVarIndex, mustCast);
+
         // and if no match, generate exception:
         generateException(mv, beanClassName, props.size());
         mv.visitMaxs(0, 0); // don't care (real values: 1,1)
@@ -441,16 +446,18 @@ public class PropertyMutatorCollector
      */
 
     private <T extends OptimizedSettableBeanProperty<T>> void _addFieldsUsingIf(MethodVisitor mv,
-            List<T> props, int loadValueCode, int beanIndex, boolean mustCast)
+            List<T> props, int loadValueCode, int localVarIndex, boolean mustCast)
     {
-        mv.visitVarInsn(ILOAD, 2); // load second arg (index)
+        mv.visitVarInsn(ALOAD, 0); // this
+        mv.visitFieldInsn(GETFIELD, MUTATOR_CLASS, "index", "I");
         Label next = new Label();
         // first: check if 'index == 0'
         mv.visitJumpInsn(IFNE, next); // "if not zero, goto L (skip stuff)"
 
         // first field accessor
-        mv.visitVarInsn(ALOAD, beanIndex); // load local for cast bean
-        mv.visitVarInsn(loadValueCode, 3);
+        mv.visitVarInsn(ALOAD, 1); // 'bean'
+        mv.visitTypeInsn(CHECKCAST, beanClassName);
+        mv.visitVarInsn(loadValueCode, 2);
         AnnotatedField field = (AnnotatedField) props.get(0).getMember();
         Type type = Type.getType(field.getRawType());
         if (mustCast) {
@@ -465,12 +472,14 @@ public class PropertyMutatorCollector
             // No comparison needed for the last entry; assumed to match
             if (i < end) {
                 next = new Label();
-                mv.visitVarInsn(ILOAD, 2); // load second arg (index)
+                mv.visitVarInsn(ALOAD, 0); // this
+                mv.visitFieldInsn(GETFIELD, MUTATOR_CLASS, "index", "I");
                 mv.visitInsn(ALL_INT_CONSTS[i]);
                 mv.visitJumpInsn(IF_ICMPNE, next);
             }
-            mv.visitVarInsn(ALOAD, beanIndex); // load bean
-            mv.visitVarInsn(loadValueCode, 3);
+            mv.visitVarInsn(ALOAD, 1); // 'bean'
+            mv.visitTypeInsn(CHECKCAST, beanClassName);
+            mv.visitVarInsn(loadValueCode, 2);
             field = (AnnotatedField) props.get(i).getMember();
             type = Type.getType(field.getRawType());
             if (mustCast) {
