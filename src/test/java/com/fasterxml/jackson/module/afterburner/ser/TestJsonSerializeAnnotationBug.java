c00f3abdc0bc39b566a708c1c0d6608f210487da
Tatu Saloranta <tatu.saloranta@iki.fi>
2015-06-25 22:59:18 -0700

    test cleanup

--- src/test/java/com/fasterxml/jackson/module/afterburner/ser/TestJsonSerializeAnnotationBug.java
+++ src/test/java/com/fasterxml/jackson/module/afterburner/ser/TestJsonSerializeAnnotationBug.java
@@ -1,21 +1,37 @@
 package com.fasterxml.jackson.module.afterburner.ser;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.module.afterburner.AfterburnerModule;
-import com.fasterxml.jackson.module.afterburner.AfterburnerTestBase;
-
 import java.math.BigDecimal;
 
-/**
- * @author Joost van de Wijgerd
- */
-public class TestJsonSerializeAnnotationBug extends AfterburnerTestBase {
-    public void testAfterburnerModule() throws JsonProcessingException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new AfterburnerModule());
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
-        String value = objectMapper.writeValueAsString(new TestObjectWithJsonSerialize(new BigDecimal("870.04")));
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+
+public class TestJsonSerializeAnnotationBug
+    extends com.fasterxml.jackson.module.afterburner.AfterburnerTestBase
+{
+    public static class TestObjectWithJsonSerialize {
+        @JsonSerialize(using = ToStringSerializer.class)
+        private final BigDecimal amount;
+
+        @JsonCreator
+        public TestObjectWithJsonSerialize(@JsonProperty("amount") BigDecimal amount) {
+            this.amount = amount;
+        }
+
+        @JsonSerialize(using = ToStringSerializer.class) @JsonProperty("amount")
+        public BigDecimal getAmount() {
+            return amount;
+        }
+    }
+
+    public void testAfterburnerModule() throws Exception
+    {
+        ObjectMapper mapper = mapperWithModule();
+
+        String value = mapper.writeValueAsString(new TestObjectWithJsonSerialize(new BigDecimal("870.04")));
         assertNotNull(value);
     }
 }
