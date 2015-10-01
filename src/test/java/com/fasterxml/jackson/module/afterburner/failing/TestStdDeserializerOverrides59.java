1f6ef257d9f352e1b336de7899888347fc3cb886
Tatu Saloranta <tatu.saloranta@iki.fi>
2015-10-01 10:48:07 -0700

    and yet more tweaking of #59 test case, confirm with 'vanilla' ObjectMapper

--- src/test/java/com/fasterxml/jackson/module/afterburner/failing/TestStdDeserializerOverrides59.java
+++ src/test/java/com/fasterxml/jackson/module/afterburner/failing/TestStdDeserializerOverrides59.java
@@ -35,13 +35,22 @@ public class TestStdDeserializerOverrides59 extends AfterburnerTestBase
     // for [module-afterburner#59]
     public void testStringDeserializerOveride59() throws Exception
     {
-        ObjectMapper mapper = mapperWithModule()
-                .registerModule(
-                        new SimpleModule("module", Version.unknownVersion())
-                        .addDeserializer(String.class, new DeAmpDeserializer())
-                );
-        String json = "{\"field\": \"value &amp; value\"}";
-        SimpleRO parsedRO = mapper.readValue(json, SimpleRO.class);
-        assertEquals("value & value", parsedRO.field);
+        final String json = "{\"field\": \"value &amp; value\"}";
+        final String EXP = "value & value";
+
+        final Module module = new SimpleModule("module", Version.unknownVersion())
+                .addDeserializer(String.class, new DeAmpDeserializer());
+
+        // First; passes with vanilla ObjectMapper
+        SimpleRO resultVanilla = new ObjectMapper()
+            .registerModule(module)
+            .readValue(json, SimpleRO.class);
+        assertEquals(EXP, resultVanilla.field);
+
+        // but then fails with Afterburner
+        SimpleRO resultAB = mapperWithModule()
+            .registerModule(module)
+            .readValue(json, SimpleRO.class);
+        assertEquals(EXP, resultAB.field);
     }
 }
