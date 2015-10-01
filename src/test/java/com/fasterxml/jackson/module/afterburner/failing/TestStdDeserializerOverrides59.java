20f1110e217895819c38217b1d63331e087025be
Tatu Saloranta <tatu.saloranta@iki.fi>
2015-10-01 10:44:01 -0700

    Fix #59 test; needs to actually register module

--- src/test/java/com/fasterxml/jackson/module/afterburner/failing/TestStdDeserializerOverrides59.java
+++ src/test/java/com/fasterxml/jackson/module/afterburner/failing/TestStdDeserializerOverrides59.java
@@ -35,10 +35,11 @@ public class TestStdDeserializerOverrides59 extends AfterburnerTestBase
     // for [module-afterburner#59]
     public void testStringDeserializerOveride59() throws Exception
     {
-        ObjectMapper mapper = mapperWithModule();
-        SimpleModule module = new SimpleModule("module", Version.unknownVersion());
-        module.addDeserializer(String.class, new DeAmpDeserializer());
-
+        ObjectMapper mapper = mapperWithModule()
+                .registerModule(
+                        new SimpleModule("module", Version.unknownVersion())
+                        .addDeserializer(String.class, new DeAmpDeserializer())
+                );
         String json = "{\"field\": \"value &amp; value\"}";
         SimpleRO parsedRO = mapper.readValue(json, SimpleRO.class);
         assertEquals("value & value", parsedRO.field);
